9db1d1b1c8d77b5bee19a0fb1d876b8de8f0adfa
Jefferson Benedito <jefiim.ad@gmail.com>
2018-05-03 10:15:23 -0300

    Task #25: cadastrar usuário

---
+++ components/register/register-service.js
@@ -0,0 +1,12 @@
+angular.module('sos-redacao').service('RegisterService', function ($http, consts) {
+    var service = this;
+
+
+    service.createUser = createUser;
+
+    function createUser(data) {
+        return $http.post(consts.api_url + '/user', data);
+    };
+
+    return service;
+});
