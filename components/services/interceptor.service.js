a2f966f31245a45f21645f5f4acc8ec3c2162766
Ordan Santos <ordan.santos@gmail.com>
2018-05-24 05:22:04 -0300

    deslogando quando o token expirar no front-end

--- components/services/interceptor.service.js
+++ components/services/interceptor.service.js
@@ -1,15 +1,33 @@
 angular.module('sos-redacao').factory('AuthInterceptor', AuthInterceptor)
-  .config(function($httpProvider) {
-    $httpProvider.interceptors.push('AuthInterceptor');
-  });
+    .config(function ($httpProvider) {
+        $httpProvider.interceptors.push('AuthInterceptor');
+    });
 
-function AuthInterceptor ($location, $q, $injector) {
-  return {
-    request: function(config) {
-      config.headers = config.headers || {};
-      authentication = $injector.get('authentication');
-      config.headers['Authorization'] = authentication.getToken();
-      return config;
+function AuthInterceptor($location, $q, $injector, $timeout) {
+    return {
+        request: function (config) {
+            config.headers = config.headers || {};
+            authentication = $injector.get('authentication');
+            config.headers['Authorization'] = authentication.getToken();
+
+
+            if ($location.path() !== '/login') {
+                if (!authentication.isLoggedIn()) {
+                    $timeout(function () {
+                        $injector.get('$state').go('login');
+                    });
+                }
+            }
+
+            if ($location.path() === '/login') {
+                if (authentication.isLoggedIn()) {
+                    $timeout(function () {
+                        $injector.get('$state').go('home');
+                    });
+                }
+            }
+
+            return config;
+        }
     }
-  }
 };
