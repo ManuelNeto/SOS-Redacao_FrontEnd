f93bcf8cbcb57d686d077c17c3db98c5b99b9c87
artxdmaster <arthur.rodrigues@ccc.ufcg.edu.br>
2018-05-31 17:39:01 -0300

    user data update

--- components/user/user-controller.js
+++ components/user/user-controller.js
@@ -1,4 +1,4 @@
-angular.module('sos-redacao').controller('UserController', function ($scope, UserService, authentication, $mdDialog, toastService) {
+angular.module('sos-redacao').controller('UserController', function ($state, $scope, UserService, authentication, $mdDialog, toastService) {
     var self = this;
 
     self.title = "Perfil";
@@ -6,22 +6,36 @@ angular.module('sos-redacao').controller('UserController', function ($scope, Use
     $scope.profile = {};
 
     $scope.enableForm = true;
+    $scope.editPass = false;
 
     $scope.setEnable = function(){
       console.log(!($scope.enableForm));
       $scope.enableForm = !($scope.enableForm);
+      $scope.editPass = false;
+    }
+
+    $scope.getNewPass = function() {
+      $scope.editPass = !($scope.editPass);
     }
 
     $scope.editarUsuario = function(profile){
 
         var success = function (response) {
             console.log(response);
-            if(response.status === 201){
+            if(response.status === 201) {
+                $state.go('user');
+            }
+            if(response.status === 200) {
+                toastService.showMessage('Perfil atualizado!');
+                $scope.setEnable();
                 $state.go('user');
             }
         };
 
         var error = function (err) {
+            if (err.status === 401) {
+                toastService.showMessage('Senha inválida.');
+            }
             console.log(err);
         };
 
@@ -35,22 +49,34 @@ angular.module('sos-redacao').controller('UserController', function ($scope, Use
        });
     };
 
-    $scope.showConfirm = function(ev) {
-    var confirm = $mdDialog.confirm()
-          .title('Você deseja confirmar a alteração nos dados?')
-          .ariaLabel('Lucky day')
-          .targetEvent(ev)
-          .ok('Confirmar')
-          .cancel('cancelar');
+    function validUpdate(profile) {
+      if (profile.pass != profile.cpass) return 'Senhas não conferem.';
+      if ($scope.editPass && profile.npass != profile.cnpass) return 'Nova senha não confere.'
+      if ($scope.editPass && !profile.npass) return 'Nova senha não pode ser vazia.'
+      return 'Sucesso';
+    }
 
-    $mdDialog.show(confirm).then(function() {
-      $scope.editarUsuario($scope.profile);
-      $scope.setEnable();
-        toastService.showMessage('Perfil atualizado!');
-    }, function() {
-       $state.go('user');
-    });
-  };
+    $scope.showConfirm = function(ev) {
+      var confirm = $mdDialog.confirm()
+            .title('Você deseja confirmar a alteração nos dados?')
+            .ariaLabel('Lucky day')
+            .targetEvent(ev)
+            .ok('Confirmar')
+            .cancel('cancelar');
+
+      $mdDialog.show(confirm).then(function() {
+        var ret = validUpdate($scope.profile);
+
+        if (ret == 'Sucesso') {
+          $scope.editarUsuario($scope.profile);
+        }
+        else {
+          toastService.showMessage(ret);
+        }
+      }, function() {
+         $state.go('user');
+      });
+    };
 
     init = function(){
       $scope.getUsuario();
