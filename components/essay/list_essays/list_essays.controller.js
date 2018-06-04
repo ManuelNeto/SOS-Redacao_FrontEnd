866e596fbcb43d5f7e9c54951f373569d182f88a
Jefferson Benedito <jefiim.ad@gmail.com>
2018-06-04 16:59:29 -0300

    Improvement - using toastService to show server response messages to user

--- components/essay/list_essays/list_essays.controller.js
+++ components/essay/list_essays/list_essays.controller.js
@@ -9,7 +9,7 @@ angular.module('sos-redacao').controller('ListEssaysController', function($state
     EssayFactory.getEssays().then(function(result){
             $scope.essays = result.data.data;
         }).catch(function(result){
-            console.log("Error");
+        toastService(result.data.message);
     });
 	}
 
