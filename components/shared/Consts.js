2f08e516544c0124f3c567b2b0b4719646d5a713
Jefferson Benedito <jefiim.ad@gmail.com>
2018-06-07 17:55:36 -0300

    change backend url

--- components/shared/Consts.js
+++ components/shared/Consts.js
@@ -4,7 +4,7 @@ angular.module('sos-redacao').service('consts', function () {
     /*Possivelmente você vai ter que trocar essa url. Não tem problema =D */
     service.api_url = "https://sosredacao.herokuapp.com";
 
-    //service.api_url = "http://localhost:8081";
+    // service.api_url = "http://localhost:8081";
 
     return service;
 });
