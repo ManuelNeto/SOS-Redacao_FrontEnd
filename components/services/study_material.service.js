6b549e15b4100e87ea90e722fadfe916e7a03099
Ordan Santos <ordan.santos@gmail.com>
2018-06-21 03:20:45 -0300

    adding pdf

--- components/services/study_material.service.js
+++
@@ -1,9 +0,0 @@
-angular.module('sos-redacao').factory('StudyMaterialFactory', function ($window, $http, $state, consts) {
-
-    return {
-        downloadStudyMaterial: function(fileName) {
-           window.open(consts.api_url + '/downloads/' +  fileName);
-        }
-
-    }
-});
