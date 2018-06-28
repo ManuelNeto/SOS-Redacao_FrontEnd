9493a9ea0972c8bf6cf1c5fb2e1b0e8d02635984
Jefferson Benedito <jefiim.ad@gmail.com>
2018-06-28 18:07:37 -0300

    Task #141 - usando notas das redações corrigidas como dados para o line chart de progresso do redator

--- components/my_progress/my_progress.controller.js
+++ components/my_progress/my_progress.controller.js
@@ -3,13 +3,30 @@ angular.module('sos-redacao').controller('MyProgressController', function($state
 
   $scope.correctedEssays = [];
   $scope.$parent.title = "Meu Progresso";
+	$scope.scorePoints = [
+		{ y: 900 },
+		{ y: 850 },
+		{ y: 950 },
+		{ y: 460 },
+		{ y: 450 },
+		{ y: 500 },
+		{ y: 480 },
+		{ y: 480 },
+		{ y: 410 },
+		{ y: 500 },
+		{ y: 150 },
+		{ y: 0 }
+	];
 
 
   getMyCorrectedEssays = function(){
 
     EssayFactory.getMyCorrectedEssays(authentication.currentUser().id).then(function(result){
             $scope.correctedEssays = result.data.data;
-            console.log(result.data.data);
+			$scope.scorePoints = fillInScoreChart($scope.correctedEssays);
+
+			chartRender();
+
         }).catch(function(result){
         toastService.showMessage(result.data.message);
     });
@@ -17,35 +34,37 @@ angular.module('sos-redacao').controller('MyProgressController', function($state
 
   init = function(){
     getMyCorrectedEssays();
-    console.log(chart);
+  };
+
+  function chartRender() {
+	  var chart = new CanvasJS.Chart("chartContainer", {
+		  animationEnabled: true,
+		  theme: "light2",
+		  axisY:{
+			  includeZero: false
+		  },
+		  axisX: {
+		  	interval: 1
+		  },
+		  data: [{
+			  type: "line",
+			  xValueFormatString:"Nota ####",
+			  dataPoints: $scope.scorePoints
+		  }]
+	  });
+
+	  chart.render();
   }
 
-  var chart = new CanvasJS.Chart("chartContainer", {
-  	animationEnabled: true,
-  	theme: "light2",
-  	axisY:{
-  		includeZero: false
-  	},
-  	data: [{
-  		type: "line",
-      xValueFormatString:"Tema ####",
-  		dataPoints: [
-  			{ y: 900 },
-  			{ y: 850 },
-  			{ y: 950 },
-  			{ y: 460 },
-  			{ y: 450 },
-  			{ y: 500 },
-  			{ y: 480 },
-  			{ y: 480 },
-  			{ y: 410 },
-  			{ y: 500 },
-  			{ y: 150 },
-  			{ y: 0 }
-  		]
-  	}]
-  });
-  chart.render();
+  function fillInScoreChart(essays) {
+	  var dataPoints = [];
+	  essays.forEach(function (essay, index) {
+		  dataPoints.push({y: essay.finalScore, x: index + 1});
+	  });
+
+	  return dataPoints;
+  }
+
 
   init();
 
