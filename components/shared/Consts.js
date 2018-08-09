99a67508bdb4c0995825bf7259da34bdf4fa9034
Jefferson Carlos Benedito <jefiim.ad@gmail.com>
2018-08-09 15:43:05 -0300

    Hotfix - sign-up button's layout and profile's image size at sign-up

--- components/shared/Consts.js
+++ components/shared/Consts.js
@@ -2,9 +2,9 @@ angular.module('sos-redacao').service('consts', function () {
     var service = this;
 
     /*Possivelmente você vai ter que trocar essa url. Não tem problema =D */
-    //service.api_url = "https://sosredacao.herokuapp.com";
+    service.api_url = "https://sosredacao.herokuapp.com";
 
-      service.api_url = "http://localhost:8081";
+      // service.api_url = "http://localhost:8081";
 
     return service;
 });
